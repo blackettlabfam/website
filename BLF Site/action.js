de2c8ff655121bec19afb24d9dec284a4cf93610
sdm03 <44570734+sdm03@users.noreply.github.com>
2020-08-07 12:54:15 +0100

    uploading website files

---
+++ BLF Site/action.js
@@ -0,0 +1,15 @@
+function myFunction() {
+    var moreText = document.getElementById("readmore");
+    var moreBtnText = document.getElementById("MoreBtn");
+    var lessBtnText = document.getElementById("LessBtn");
+
+    if (moreBtnText.style.display === "none") {
+      moreBtnText.style.display = "inline";
+      moreText.style.display = "none";
+      lessBtnText.style = "none";
+    } else {
+      moreBtnText.style.display = "none";
+      moreText.style.display = "inline";
+      lessBtnText.style.display = "inline"; 
+    }
+  }
