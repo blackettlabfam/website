65705361afdaf19f3f8d79f8a6d6341eae694a64
Ose Pedro <zuverliantellabrach@hotmail.com>
2020-10-12 00:30:59 +0100

    redirect() now reads the form directly, rather than parsing the GET query.

--- view.js
+++ view.js
@@ -1,32 +1,11 @@
-// Get query parameter
-// Source: https://css-tricks.com/snippets/javascript/get-url-variables/
-function getQueryParameter(variable) {
-    var query = window.location.search.substring(1);
-    var vars = query.split("&");
-    for (var i = 0; i < vars.length; i++) {
-      var pair = vars[i].split("=");
-      if (pair[0] == variable) {
-        return pair[1];
-      }
-    }
-    return (false);
-  }
-
-  function redirect() {
-
-  // Capture the `ukbased` query parameter from the URL.
-  var ukbased = getQueryParameter('ukbased')
-  var blackheritage = getQueryParameter('blackheritage')
-  var physicsqual = getQueryParameter('physicsquals')
+function redirect(form) {
+  var ukbased = form.elements.ukbased.value;
+  var blackheritage = form.elements.blackheritage.value;
+  var physicsqual = form.elements.physicsqual.value;
 
  if(ukbased == 'No' || blackheritage == 'No' || physicsqual == 'No' ) {
     window.location.href = 'redirect.html';
-
   } else {
-
     window.location.href = 'welcome.html';
-
   }
-
-
-  }
+}
