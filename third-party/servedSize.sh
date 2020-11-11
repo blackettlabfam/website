e6ef35957297ed64ed2f4d5c3630dd442891c614
Ose Pedro <zuverliantellabrach@hotmail.com>
2020-11-11 23:59:03 +0000

    Removed servedSize.sh from its own calculation

--- third-party/servedSize.sh
+++ third-party/servedSize.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-find -type f \! -name '*.map' | xargs du -b | perl -e '$s=0; while(<>) {/^(\d+)/; $s+=$1;} $kb=$s/1000; print $kb."KB\n";'
+find -mindepth 2 -type f \! -name '*.map' | xargs du -b | perl -e '$s=0; while(<>) {/^(\d+)/; $s+=$1;} $kb=$s/1000; print $kb."KB\n";'
